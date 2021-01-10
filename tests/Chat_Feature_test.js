0b907b51c7f957377e83290ce2839a585982e1ef
Pete Looney <petelooney81@gmail.com>
2021-01-10 15:57:47 -0600

    Chat Feature Tests - Creating Multiple Chat Groups and Sending a Message

--- tests/Chat_Feature_test.js
+++ tests/Chat_Feature_test.js
@@ -11,28 +11,28 @@ const login = (I) => {
     I.click('.signup-button'); // clicks the signup button in the form...should say login on this page
 }
 
-Scenario('Login and Create A New Chat Group', ({ I }) => {
-    const groupNames = ['Python Crazies', 'Pete\'s Coding Crew', 'Pair questions chat', 'Misc'];
-    //addedGroup${groupInfo.name}
-    login(I);
-    I.seeElement('.message-form'); //simple check to see if we are actually on the chat page
-    I.wait(2);
-    I.click('.hide-side-button') //click the side button
-    I.see('Current Groups', 'h3'); //sees that the side button div is open
+// Scenario('Login and Create A New Chat Group', ({ I }) => {
+//     const groupNames = ['Python Crazies', 'Pete\'s Coding Crew', 'Pair questions chat', 'Misc'];
+//     //addedGroup${groupInfo.name}
+//     login(I);
+//     I.seeElement('.message-form'); //simple check to see if we are actually on the chat page
+//     I.wait(2);
+//     I.click('.hide-side-button') //click the side button
+//     I.see('Current Groups', 'h3'); //sees that the side button div is open
 
-    //Loop to create a new group for each groupName in the groupNames array;
-    groupNames.forEach(groupName => {
-        I.fillField('#newGroupForm', groupName); //fills in form with specified name
-        I.click('Add'); //adds the group by clicking the Add button
-    })
-});
+//     //Loop to create a new group for each groupName in the groupNames array;
+//     groupNames.forEach(groupName => {
+//         I.fillField('#newGroupForm', groupName); //fills in form with specified name
+//         I.click('Add'); //adds the group by clicking the Add button
+//     })
+// });
 
 Scenario('Click on a chat group', ({I}) => {
     login(I);
-    I.click('Chat');
+    I.click('Chat'); //navigate to the chat page
     I.click('.hide-side-button') //click the side button
-    I.click(locate(`.addedGroupMisc`).withText('Misc'));
+    I.click(locate(`.addedGroupMisc`).withText('Misc')); //click on the right group to load
     I.wait(2);
-    I.fillField('.message-form', 'Testing with CodeCept');
+    I.fillField('.text-input', 'Testing with CodeCept'); //Type
     I.click('.fa-paper-plane');
 });
