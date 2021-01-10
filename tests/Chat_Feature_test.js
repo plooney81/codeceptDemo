ad91389205af4bbee9de66220aac163c9d0274e5
Pete Looney <petelooney81@gmail.com>
2021-01-10 15:38:14 -0600

    Chat Feature Tests - Creating Multiple Chat Groups

--- tests/Chat_Feature_test.js
+++ tests/Chat_Feature_test.js
@@ -1,6 +1,6 @@
 Feature('Chat Feature');
 
-Scenario('Login and Create A New Chat Group', ({ I }) => {
+const login = (I) => {
     I.amOnPage('/'); //opens the site
     I.see('Pair'); //Checks to see if it has loaded properly
     I.click('Login'); //Clicks the signup link
@@ -9,16 +9,32 @@ Scenario('Login and Create A New Chat Group', ({ I }) => {
     I.fillField('#formBasicEmail', 'test100@test.com'); //types in a test email 
     I.fillField('#formBasicPassword', secret('123456')); // types in a test password
     I.click('.signup-button'); // clicks the signup button in the form...should say login on this page
-    const groupName = 'Demo Group';
-    I.seeElement('.message-form'); //should take us to the chat page
-    I.wait(5);
+}
+
+Scenario('Login and Create A New Chat Group', ({ I }) => {
+    const groupNames = ['Python Crazies', 'Pete\'s Coding Crew', 'Pair questions chat', 'Misc'];
+    //addedGroup${groupInfo.name}
+    login(I);
+    I.seeElement('.message-form'); //simple check to see if we are actually on the chat page
+    I.wait(2);
     I.click('.hide-side-button') //click the side button
     I.see('Current Groups', 'h3'); //sees that the side button div is open
-    I.fillField('#newGroupForm', groupName); //adds a new chat group with the name
-    I.wait(5);
-    I.click('Add');
-    I.wait(5);
-    I.click(groupName, 'span')
-    I.fillField('.message-form', "Hello World");
+
+    //Loop to create a new group for each groupName in the groupNames array;
+    groupNames.forEach(groupName => {
+        I.fillField('#newGroupForm', groupName); //fills in form with specified name
+        I.click('Add'); //adds the group by clicking the Add button
+        I.wait(5);
+        I.click('Home');
+        I.click('Chat');
+        // I.seeElement(groupName, `.addedGroup`); //checks that the user can now see the added group
+    })
+    
+    // I.fillField('.message-form', "Hello World");
     
 });
+
+// Scenario('Click on a chat group', ({I}) => {
+//     I.amOnPage('/login');
+
+// });
