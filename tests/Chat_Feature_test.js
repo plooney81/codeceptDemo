479676335ae8f979941e8a79e021d02b210ba2c8
Pete Looney <petelooney81@gmail.com>
2021-01-11 11:15:16 -0600

    Chat Feature Tests - Creating Multiple Chat Groups and Sending a Message

--- tests/Chat_Feature_test.js
+++ tests/Chat_Feature_test.js
@@ -11,28 +11,31 @@ const login = (I) => {
     I.click('.signup-button'); // clicks the signup button in the form...should say login on this page
 }
 
-// Scenario('Login and Create A New Chat Group', ({ I }) => {
-//     const groupNames = ['Python Crazies', 'Pete\'s Coding Crew', 'Pair questions chat', 'Misc'];
-//     //addedGroup${groupInfo.name}
-//     login(I);
-//     I.seeElement('.message-form'); //simple check to see if we are actually on the chat page
-//     I.wait(2);
-//     I.click('.hide-side-button') //click the side button
-//     I.see('Current Groups', 'h3'); //sees that the side button div is open
-
-//     //Loop to create a new group for each groupName in the groupNames array;
-//     groupNames.forEach(groupName => {
-//         I.fillField('#newGroupForm', groupName); //fills in form with specified name
-//         I.click('Add'); //adds the group by clicking the Add button
-//     })
-// });
-
-Scenario('Click on a chat group', ({I}) => {
+Scenario('Login and Create A New Chat Group', ({ I }) => {
+    const groupNames = ['Python Crazies', 'Pete\'s Coding Crew', 'Pair questions chat', 'Misc'];
+    //addedGroup${groupInfo.name}
     login(I);
-    I.click('Chat'); //navigate to the chat page
-    I.click('.hide-side-button') //click the side button
-    I.click(locate(`.addedGroupMisc`).withText('Misc')); //click on the right group to load
+    I.seeElement('.message-form'); //simple check to see if we are actually on the chat page
     I.wait(2);
-    I.fillField('.text-input', 'Testing with CodeCept'); //types a message
-    I.click(locate('.fa-paper-plane')); //sends the message
+    I.click('.hide-side-button') //click the side button
+    I.see('Current Groups', 'h3'); //sees that the side button div is open
+
+    //Loop to create a new group for each groupName in the groupNames array;
+    groupNames.forEach(groupName => {
+        I.fillField('#newGroupForm', groupName); //fills in form with specified name
+        I.click('Add'); //adds the group by clicking the Add button
+        I.click('Home');
+        I.click('Chat');
+        I.see(locate(`.addedGroup${groupName}`).withText(groupName)); //checks that we have the new group added under the right section
+    })
 });
+
+// Scenario('Click on a chat group', ({I}) => {
+//     login(I);
+//     I.click('Chat'); //navigate to the chat page
+//     I.click('.hide-side-button') //click the side button
+//     I.click(locate(`.addedGroupMisc`).withText('Misc')); //click on the right group to load
+//     I.wait(2);
+//     I.fillField('.text-input', 'Testing with CodeCept'); //types a message
+//     I.click(locate('.fa-paper-plane')); //sends the message
+// });
